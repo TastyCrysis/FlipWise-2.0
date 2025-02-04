5ce6f60f1f8ad4f40490370d02ce1c0ab3c45ec8
Alexander <mail@alexandersaupe.de>
2025-02-04 11:33:12 +0100

    Add styling for flashcards

--- components/StyledCard.js
+++ components/StyledCard.js
@@ -1,27 +1,70 @@
 import { flashcards } from "@/lib/db/flashcards";
 import { collections } from "@/lib/db/collections";
+import styled from "styled-components";
+
+const CardList = styled.ul`
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+`;
+
+const Card = styled.li`
+  max-width: 550px;
+  background: #fff;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  margin: 15px 32px;
+  padding: 15px;
+  transition: transform 0.2s;
+
+  &:hover {
+    transform: scale(1.02);
+  }
+`;
+
+const CardContent = styled.article`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Question = styled.p`
+  font-weight: bold;
+  margin: 0;
+`;
+
+const Answer = styled.p`
+  color: #555;
+  margin: 5px 0;
+`;
+
+const CollectionTitle = styled.p`
+  text-align: right;
+  font-style: italic;
+  color: #888;
+  margin: 0 0 7px 0;
+`;
 
 export default function StyledCard() {
   return (
     <>
-      <ul>
+      <CardList>
         {flashcards.map((flashcard) => {
           const collection = collections.find(
             (collection) => collection.id === flashcard.collectionId
           );
           return (
-            <li key={flashcard.id}>
-              <article>
-                <p>{collection.title}</p>
+            <Card key={flashcard.id}>
+              <CardContent>
+                <CollectionTitle>{collection.title}</CollectionTitle>
                 <div>
-                  <p>{flashcard.question}</p>
-                  <p>{flashcard.answer}</p>
+                  <Question>{flashcard.question}</Question>
+                  <Answer>{flashcard.answer}</Answer>
                 </div>
-              </article>
-            </li>
+              </CardContent>
+            </Card>
           );
         })}
-      </ul>
+      </CardList>
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 import StyledCard from "@/components/StyledCard";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
 export default function index() {
   return (
-    <div>
-      <h1>Flipwise App</h1>
+    <>
+      <Container>
+        <h1>Flipwise App</h1>
+      </Container>
       <StyledCard />
-    </div>
+    </>
   );
 }
