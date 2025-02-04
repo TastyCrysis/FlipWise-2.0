4f991a7dec20db06ba505978373d2eb9e0fc3aa1
Alexander <mail@alexandersaupe.de>
2025-02-04 10:53:33 +0100

    Map over databases and render a list of flashcards

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,28 @@
-import flashcards from "@/lib/db/flashcards";
+import { flashcards } from "@/lib/db/flashcards";
+import { collections } from "@/lib/db/collections";
 
 export default function index() {
   return (
     <div>
       <h1>Flipwise App</h1>
+      <ul>
+        {flashcards.map((flashcard) => {
+          const collection = collections.find(
+            (collection) => collection.id === flashcard.collectionId
+          );
+          return (
+            <li key={flashcard.id}>
+              <article>
+                <p>{collection.title}</p>
+                <div>
+                  <p>{flashcard.question}</p>
+                  <p>{flashcard.answer}</p>
+                </div>
+              </article>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
