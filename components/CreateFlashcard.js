fb5748b2389508de518da868a94e9c111736118b
Herr <g.kerber@posteo.it>
2025-02-06 12:07:04 +0100

    add styling for crate form

--- components/CreateFlashcard.js
+++ components/CreateFlashcard.js
@@ -1,9 +1,21 @@
 import FlashcardForm from "./FlashcardForm";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: solid 2px;
+  border-radius: 8px;
+  margin: 4px 48px 4px 48px;
+  padding: 0 0 16px 0;
+`;
 
 export default function CreateFlashcard({ handleCreateFlashcard }) {
   return (
-    <div>
+    <Container>
+      <h3>Create a new Flashcard</h3>
       <FlashcardForm onSubmit={handleCreateFlashcard} />
-    </div>
+    </Container>
   );
 }
--- components/FlashcardForm.js
+++ components/FlashcardForm.js
@@ -1,4 +1,12 @@
 import { collections } from "@/lib/db/collections";
+import styled from "styled-components";
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 4px;
+`;
 
 export default function FlashcardForm({ onSubmit }) {
   function handleSubmit(event) {
@@ -12,44 +20,40 @@ export default function FlashcardForm({ onSubmit }) {
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="question">Question:</label>
-          <input
-            id="question"
-            type="text"
-            name="question"
-            placeholder="Question*"
-            required
-          ></input>
-          <label htmlFor="answer">Answer:</label>
-          <input
-            id="answer"
-            type="text"
-            name="answer"
-            placeholder="Answer*"
-            required
-          ></input>
-          <label htmlFor="collections-select">Collection:</label>
-          <select
-            name="collectionId"
-            id="collections-select"
-            defaultValue=""
-            required
-          >
-            <option value="" disabled>
-              --Please select a collection--
-            </option>
-            {collections.map((collection) => (
-              <option key={collection.id} value={collection.id}>
-                {collection.title}
-              </option>
-            ))}
-          </select>
-          <button type="submit">create</button>
-        </div>
-      </form>
-    </div>
+    <StyledForm onSubmit={handleSubmit}>
+      <label htmlFor="question">Question:</label>
+      <input
+        id="question"
+        type="text"
+        name="question"
+        placeholder="Question*"
+        required
+      ></input>
+      <label htmlFor="answer">Answer:</label>
+      <input
+        id="answer"
+        type="text"
+        name="answer"
+        placeholder="Answer*"
+        required
+      ></input>
+      <label htmlFor="collections-select">Collection:</label>
+      <select
+        name="collectionId"
+        id="collections-select"
+        defaultValue=""
+        required
+      >
+        <option value="" disabled>
+          --Please select a collection--
+        </option>
+        {collections.map((collection) => (
+          <option key={collection.id} value={collection.id}>
+            {collection.title}
+          </option>
+        ))}
+      </select>
+      <button type="submit">create</button>
+    </StyledForm>
   );
 }
