10e7c79a6fc4dd4e773a15fe6808192bb06e90b3
Philipp Gampe <tastycrysis@gmail.com>
2025-02-06 17:50:24 +0100

    solve github comments

--- components/FlashcardForm.js
+++ components/FlashcardForm.js
@@ -8,6 +8,16 @@ const StyledForm = styled.form`
   gap: 4px;
 `;
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: solid 2px;
+  border-radius: 8px;
+  margin: 4px 48px 4px 48px;
+  padding: 0 0 16px 0;
+`;
+
 export default function FlashcardForm({ onSubmit }) {
   function handleSubmit(event) {
     event.preventDefault();
@@ -18,40 +28,43 @@ export default function FlashcardForm({ onSubmit }) {
   }
 
   return (
-    <StyledForm onSubmit={handleSubmit}>
-      <label htmlFor="question">Question:</label>
-      <input
-        id="question"
-        type="text"
-        name="question"
-        placeholder="Question*"
-        required
-      ></input>
-      <label htmlFor="answer">Answer:</label>
-      <input
-        id="answer"
-        type="text"
-        name="answer"
-        placeholder="Answer*"
-        required
-      ></input>
-      <label htmlFor="collections-select">Collection:</label>
-      <select
-        name="collectionId"
-        id="collections-select"
-        defaultValue=""
-        required
-      >
-        <option value="" disabled>
-          --Please select a collection--
-        </option>
-        {collections.map((collection) => (
-          <option key={collection.id} value={collection.id}>
-            {collection.title}
+    <Container>
+      <h3>Create a new Flashcard</h3>
+      <StyledForm onSubmit={handleSubmit}>
+        <label htmlFor="question">Question:</label>
+        <input
+          id="question"
+          type="text"
+          name="question"
+          placeholder="Question*"
+          required
+        />
+        <label htmlFor="answer">Answer:</label>
+        <input
+          id="answer"
+          type="text"
+          name="answer"
+          placeholder="Answer*"
+          required
+        />
+        <label htmlFor="collections-select">Collection:</label>
+        <select
+          name="collectionId"
+          id="collections-select"
+          defaultValue=""
+          required
+        >
+          <option value="" disabled>
+            --Please select a collection--
           </option>
-        ))}
-      </select>
-      <button type="submit">create</button>
-    </StyledForm>
+          {collections.map((collection) => (
+            <option key={collection.id} value={collection.id}>
+              {collection.title}
+            </option>
+          ))}
+        </select>
+        <button type="submit">create</button>
+      </StyledForm>
+    </Container>
   );
 }
