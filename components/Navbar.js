b693630e5b3c43371e2dd923f81a3b736d2cb6c3
Philipp Gampe <tastycrysis@gmail.com>
2025-02-12 16:09:45 +0100

    delete import for useeffect

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Modal from "@/components/Modal";
 import FlashcardForm from "@/components/FlashcardForm";
 
