1d354382d69ff0135438525f41785819721da5af
Philipp Gampe <tastycrysis@gmail.com>
2025-02-14 13:13:09 +0100

    fix error message

--- components/Elements/Arrow_redo-dot.js
+++ components/Elements/Arrow_redo-dot.js
@@ -7,9 +7,9 @@ export default function ArrowRedoDot() {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       style={{ transform: "rotate(90deg)" }}
     >
       <circle cx="12" cy="17" r="1" />
