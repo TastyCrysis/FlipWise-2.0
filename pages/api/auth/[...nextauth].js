0750846c917587bace17437380793c4ddf1f7e81
Herr <g.kerber@posteo.it>
2025-02-26 15:47:18 +0100

    add log in with username and password

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,65 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions = {
   providers: [
-    process.env.VERCEL_ENV !== "preview" &&
-      GithubProvider({
-        clientId: process.env.GITHUB_ID,
-        clientSecret: process.env.GITHUB_SECRET,
-      }),
+    process.env.VERCEL_ENV === "preview"
+      ? CredentialsProvider({
+          name: "credentials",
+          credentials: {
+            username: {
+              label: "Username",
+              type: "text",
+              placeholder: "username",
+            },
+            password: { label: "Password", type: "password" },
+          },
+          async authorize(credentials) {
+            if (
+              credentials.username === "fisch" &&
+              credentials.password === "fisch"
+            ) {
+              return {
+                name: "Neuer Fisch",
+                email: "test@example.com",
+                id: "a1b2c3d4",
+              };
+            } else {
+              return null;
+            }
+          },
+        })
+      : process.env.VERCEL_ENV !== "preview" &&
+        GithubProvider({
+          clientId: process.env.GITHUB_ID,
+          clientSecret: process.env.GITHUB_SECRET,
+        }),
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "username",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (
+          credentials.username === "fisch" &&
+          credentials.password === "fisch"
+        ) {
+          return {
+            name: "Neuer Fisch",
+            email: "test@example.com",
+            id: "a1b2c3d4",
+          };
+        } else {
+          return null;
+        }
+      },
+    }),
   ],
 
   //extension of the session object with the token / userId
