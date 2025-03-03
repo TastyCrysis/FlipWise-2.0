6e7b66bc1421258f5ae390b2f315744f1d61a92f
Philipp Gampe <tastycrysis@gmail.com>
2025-03-03 09:57:33 +0100

    fix auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -43,6 +43,12 @@ export const authOptions = {
       return session;
     },
   },
+
+  site:
+    process.env.NEXTAUTH_URL ||
+    (process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}`
+      : "http://localhost:3000"),
 };
 
 export default NextAuth(authOptions);
